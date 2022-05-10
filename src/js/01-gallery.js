fd7c00224908d6a751283394189ba259cee5c449
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-05-10 13:30:55 +0300

    додає ДЗ№1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const imgDivContainerEl = document.querySelector(".gallery");
+
+function makeImgCardMarkup({ preview, original,description} ) {           
+    return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}"
+  alt="${description}" />
+</a>`};
+  
+function makeImgGalleryMarkup(Items) {
+    return Items
+    .map(makeImgCardMarkup)
+    .join('');
+}
+const markup = makeImgGalleryMarkup(galleryItems);
+
+
+
+imgDivContainerEl.insertAdjacentHTML('beforeend', markup);
+
+
+var lightbox = new SimpleLightbox('.gallery__item', {
+    
+    /* options */
+    captionsData: 'alt',
+    captionDelay: 250,
+});
