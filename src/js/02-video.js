285793f701dde93dd6f88d5b70b58a4e97bc1b8b
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-05-10 20:00:23 +0300

    додає ДЗ№2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+ 
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
 
+const dataTime = localStorage.getItem(STORAGE_KEY);
+
+if (dataTime){
+    player.setCurrentTime(dataTime).
+    then(function (seconds) {
+        seconds = 0;
+    })
+        .catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                 break;
+
+            default:
+                 break;
+        }
+    });
+};
+
+ player.on('timeupdate', throttle(onTimeupdate, 1000) );
+    
+        function onTimeupdate(event) {
+            localStorage.setItem(STORAGE_KEY, event.seconds);
+                     
+        }
