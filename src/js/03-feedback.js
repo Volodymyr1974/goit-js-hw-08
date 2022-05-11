c6b91190fb7be32b89becd06f0a495c3bf2d1c58
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-05-11 07:53:02 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+{/* <form class="feedback-form" autocomplete="off">
+      <label>
+        Email
+        <input type="email" name="email" autofocus />
+      </label>
+      <label>
+        Message
+        <textarea name="message" rows="8"></textarea>
+      </label>
+      <button type="submit">Submit</button>
+    </form> */}
+// import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+form.addEventListener('input', onInput);
+function onInput(e) {
+    console.log(e.currrentTarget);
+    console.log(e.target);
+};
+form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+     const {
+        elements: { email, message },
+     } = e.currentTarget;
+     console.log(email.value); console.log(message.value);
+    console.log(e.currrentTarget);
+    console.log(e.target);
+};
