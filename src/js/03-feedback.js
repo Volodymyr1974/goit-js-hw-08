055fd405635e294b0cfb2661a3f6df5b7fb8f948
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-05-12 00:49:32 +0300

    додає рішення ДЗ№3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,50 @@
-{/* <form class="feedback-form" autocomplete="off">
-      <label>
-        Email
-        <input type="email" name="email" autofocus />
-      </label>
-      <label>
-        Message
-        <textarea name="message" rows="8"></textarea>
-      </label>
-      <button type="submit">Submit</button>
-    </form> */}
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-console.log(form);
-form.addEventListener('input', onInput);
-function onInput(e) {
-    console.log(e.currrentTarget);
-    console.log(e.target);
+const STORAGE_KEY = 'feedback-form-state';
+
+
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+  input:document.querySelector('input'),
 };
-form.addEventListener('submit', onSubmit);
+
+
+populateEmailMassage();
+
+
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+
+function onInput(e) {
+  let formData = localStorage.getItem("STORAGE_KEY");
+  if (formData) { formData = JSON.parse(localStorage.getItem("STORAGE_KEY")); } else { formData = {}; };
+
+
+  formData[e.target.name] = e.target.value;
+ 
+  localStorage.setItem("STORAGE_KEY", JSON.stringify(formData));
+};
+
+
+function populateEmailMassage() {
+   const parsedformData = JSON.parse( localStorage.getItem("STORAGE_KEY"));
+ 
+  if (parsedformData) {
+    refs.input.value = parsedformData['email'] || '';
+    refs.textarea.value = parsedformData['message'] || '';
+  };
+  
+};
+
+
+refs.form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
     e.preventDefault();
-     const {
-        elements: { email, message },
-     } = e.currentTarget;
-     console.log(email.value); console.log(message.value);
-    console.log(e.currrentTarget);
-    console.log(e.target);
+   
+  const newFormData = JSON.parse( localStorage.getItem("STORAGE_KEY"));
+  console.log(newFormData);
+  e.currentTarget.reset();
+  localStorage.removeItem("STORAGE_KEY");  
 };
